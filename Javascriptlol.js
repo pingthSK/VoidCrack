c8b3204b03ac281a24c472f03ba16df6b16b3e15
pingthSK <141994287+pingthSK@users.noreply.github.com>
2023-10-23 16:31:36 +0700

    Add files via upload

---
+++ Javascriptlol.js
@@ -0,0 +1,25 @@
+/* Place your JavaScript in this file */
+let MysCoin = 0
+
+var code = document.getElementById("code").value;
+let x = Math.floor(Math.random() * 101);
+    let y = Math.floor(Math.random() * 101);
+    let result = x*y
+    document.getElementById("demo").innerHTML = result;
+
+
+
+function myFunction(){
+    var code = document.getElementById("code").value;
+    let result = parseInt(code);
+    If (code == result){
+	MysCoin = MysCoin+1
+        document.getElementById("coin").innerHTML = Myscoin;
+    }
+
+    let x = Math.floor(Math.random() * 101);
+    let y = Math.floor(Math.random() * 101);
+    let result = x*y
+    document.getElementById("demo").innerHTML = result;
+	    
+}
